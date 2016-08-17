ba30f9218f079947ddb7a3979aab622ef5a01f5c
Drew Hollenback <drewsee26@gmail.com>
2016-08-16 21:04:31 -0700

    Assignment 11 DOM Scripting Album View

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,21 @@ var albumMarconi = {
     ]
 };
 
+var albumMonet = {
+    title: 'Poppies',
+    artist: 'Claude Monet',
+    label: 'EM',
+    year: '1873',
+    albumArtUrl: 'assets/images/album_covers/jab8_monet_001f.png',
+    songs: [
+         { title: 'Flowers', duration: '1:20' },
+         { title: 'Blooms', duration: '4:01' },
+         { title: 'Not quite lillies', duration: '3:19'},
+         { title: 'Smell so good', duration: '3:59' },
+         { title: 'High', duration: '2:00'}
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
       '<tr class="album-view-song-item">'
@@ -40,29 +55,37 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
-     // #2
-     albumTitle.firstChild.nodeValue = album.title;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
+    albumTitle.firstChild.nodeValue = album.title;
+    albumArtist.firstChild.nodeValue = album.artist;
+    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+    albumImage.setAttribute('src', album.albumArtUrl);
  
-     // #3
-     albumSongList.innerHTML = '';
+    albumSongList.innerHTML = '';
  
-     // #4
-     for (var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-     }
+    for (var i = 0; i < album.songs.length; i++) {
+        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+    }
  };
  
 window.onload = function() {
-     setCurrentAlbum(albumPicasso);
+    setCurrentAlbum(albumPicasso);
+    
+    var albums = [albumPicasso, albumMarconi, albumMonet];
+    var index = 1;
+    albumImage.addEventListener("click", function(event)) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if (index == albums.length) {
+            index = 0;
+        }
+    };
 };
+
